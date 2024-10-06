b38f84753f20b96ca5e5b4a9b836be264c21002a
brainfuck <noah1999@qq.com>
2024-10-06 23:10:50 +0800

    encode info hash

--- src/e_encoded/mod.rs
+++ src/e_encoded/mod.rs
@@ -5,16 +5,17 @@ use serde_json::{Map, Value};
 
 pub struct MyBEncodedBuf {
     pub pos: usize,
-    pub string_buf: Vec<u8>,
+    pub inner_buf: Vec<u8>,
+    pub outer_buf: Vec<u8>,
 }
 type MyBEncodedResult<T> = Result<T, Box<dyn Error>>;
 impl MyBEncodedBuf {
     pub fn get_current_slice(&self) -> &[u8] {
-        let a = &self.string_buf[self.pos..];
+        let a = &self.inner_buf[self.pos..];
         a
     }
     pub fn len_bound(&self) -> usize {
-        self.string_buf.len()
+        self.inner_buf.len()
     }
     pub fn read(&mut self) -> MyBEncodedResult<u8> {
         let ret = self.peek()?;
@@ -26,7 +27,7 @@ impl MyBEncodedBuf {
         if self.pos > self.len_bound() {
             return Err("read".into());
         }
-        let chars = self.string_buf.get(self.pos).cloned();
+        let chars = self.inner_buf.get(self.pos).cloned();
         let ret = chars.ok_or("peek")?;
 
         Ok(ret)
@@ -52,7 +53,7 @@ impl MyBEncodedBuf {
         let a = data.split_at(idx);
         Ok(a)
     }
-    pub fn parse_str(&mut self) -> MyBEncodedResult<Value> {
+    pub fn decode_str(&mut self) -> MyBEncodedResult<Value> {
         let a = self.split_by(b':')?;
         let aa = String::from_utf8_lossy(a.0).to_string();
         let n = aa.parse::<usize>().expect(&format!("parse_str {}", aa));
@@ -61,21 +62,40 @@ impl MyBEncodedBuf {
 
         Ok(s)
     }
-    pub fn parse_integer(&mut self) -> MyBEncodedResult<Value> {
+    pub fn encode_str(&mut self, s: &str) -> MyBEncodedResult<()> {
+        let len = s.as_bytes().len().to_string();
+        self.outer_buf.extend_from_slice(len.as_bytes());
+        self.outer_buf.push(b':');
+        self.outer_buf.extend_from_slice(s.as_bytes());
+
+        Ok(())
+    }
+    pub fn decode_integer(&mut self) -> MyBEncodedResult<Value> {
         self.step(1)?;
 
         let pair = self.split_by(b'e')?;
         let bb = String::from_utf8_lossy(pair.1).to_string();
         let aa = String::from_utf8_lossy(pair.0).to_string();
-        let n = aa
-            .parse::<i64>()
-            .expect(&format!("parse_integer [{}] [{}] [{}]", aa, bb,String::from_utf8_lossy(&self.string_buf)));
+        let n = aa.parse::<i64>().expect(&format!(
+            "parse_integer [{}] [{}] [{}]",
+            aa,
+            bb,
+            String::from_utf8_lossy(&self.inner_buf)
+        ));
 
         self.step(pair.0.len() + 1)?;
 
         Ok(n.into())
     }
-    pub fn parse_list(&mut self) -> MyBEncodedResult<Value> {
+    pub fn encode_integer(&mut self, s: i64) -> MyBEncodedResult<()> {
+        self.outer_buf.push(b'i');
+
+        self.outer_buf.extend_from_slice(s.to_string().as_bytes());
+        self.outer_buf.push(b'e');
+
+        Ok(())
+    }
+    pub fn decode_list(&mut self) -> MyBEncodedResult<Value> {
         self.step(1)?;
         let mut v = vec![];
         while let Ok(c) = self.peek() {
@@ -86,7 +106,7 @@ impl MyBEncodedBuf {
                 }
 
                 _ => {
-                    let value = self.parse()?;
+                    let value = self.decode()?;
                     v.push(value);
                     continue;
                 }
@@ -95,7 +115,17 @@ impl MyBEncodedBuf {
 
         Ok(v.into())
     }
-    pub fn parse_dict(&mut self) -> MyBEncodedResult<Value> {
+    pub fn encode_list(&mut self, s: &Vec<Value>) -> MyBEncodedResult<()> {
+        self.outer_buf.push(b'l');
+
+        s.iter().for_each(|item| {
+            let _ = self.encode(item);
+        });
+        self.outer_buf.push(b'e');
+
+        Ok(())
+    }
+    pub fn decode_dict(&mut self) -> MyBEncodedResult<Value> {
         self.step(1)?;
         let mut v = Map::new();
         while let Ok(c) = self.peek() {
@@ -106,7 +136,7 @@ impl MyBEncodedBuf {
                 }
 
                 _ => {
-                    let entry = self.parse_dict_entry()?;
+                    let entry = self.decode_dict_entry()?;
                     v.insert(entry.0, entry.1);
                 }
             };
@@ -114,18 +144,30 @@ impl MyBEncodedBuf {
 
         Ok(v.into())
     }
-    pub fn parse_dict_entry(&mut self) -> MyBEncodedResult<(String, Value)> {
-        let key = self.parse_dict_entry_key()?;
-        let value = self.parse()?;
+    pub fn encode_dict(&mut self, s: &Map<String, Value>) -> MyBEncodedResult<()> {
+        self.outer_buf.push(b'd');
+
+        s.iter().for_each(|item| {
+            let _ = self.encode_str(item.0);
+
+            let _ = self.encode(item.1);
+        });
+        self.outer_buf.push(b'e');
+
+        Ok(())
+    }
+    pub fn decode_dict_entry(&mut self) -> MyBEncodedResult<(String, Value)> {
+        let key = self.decode_dict_entry_key()?;
+        let value = self.decode()?;
         // println!("entry {:?}",(&key,&value));
         Ok((key, value))
     }
 
-    pub fn parse_dict_entry_key(&mut self) -> MyBEncodedResult<String> {
+    pub fn decode_dict_entry_key(&mut self) -> MyBEncodedResult<String> {
         let c = self.peek()?;
         match c {
             b'0'..=b'9' => {
-                let value = self.parse_str()?;
+                let value = self.decode_str()?;
                 Ok(value.as_str().unwrap().to_owned())
             }
             _ => panic!(
@@ -135,39 +177,50 @@ impl MyBEncodedBuf {
             ),
         }
     }
-    pub fn parse(&mut self) -> MyBEncodedResult<Value> {
+    pub fn decode(&mut self) -> MyBEncodedResult<Value> {
         let c = self.peek()?;
         let a = match c {
             b'i' => {
-                let n = self.parse_integer()?;
+                let n = self.decode_integer()?;
                 n
             }
             b'l' => {
-                let n = self.parse_list()?;
+                let n = self.decode_list()?;
                 n
             }
             b'd' => {
-                let n = self.parse_dict()?;
+                let n = self.decode_dict()?;
                 n
             }
 
             b'0'..=b'9' => {
-                let n = self.parse_str()?;
+                let n = self.decode_str()?;
                 n
             }
 
             _ => {
-                panic!("Unhandled encoded value: {:?} {}", self.string_buf, c)
+                panic!("Unhandled encoded value: {:?} {}", self.inner_buf, c)
             }
         };
         Ok(a)
+    }
+    pub fn encode(&mut self, value: &Value) -> MyBEncodedResult<()> {
+        let a = match value {
+            Value::Number(number) => self.encode_integer(number.as_i64().unwrap()),
+            Value::String(s) => self.encode_str(s),
+            Value::Array(vec) => self.encode_list(&vec),
+            Value::Object(map) => self.encode_dict(&map),
+            _ => panic!("encode {}", value),
+        };
+        a
     }
 }
 impl From<&str> for MyBEncodedBuf {
     fn from(value: &str) -> Self {
         Self {
             pos: 0,
-            string_buf: value.into(),
+            inner_buf: value.into(),
+            outer_buf: vec![],
         }
     }
 }
@@ -175,7 +228,8 @@ impl From<&String> for MyBEncodedBuf {
     fn from(value: &String) -> Self {
         Self {
             pos: 0,
-            string_buf: value.as_bytes().to_vec(),
+            inner_buf: value.as_bytes().to_vec(),
+            outer_buf: vec![],
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use bittorrent_starter_rust::MyBEncodedBuf;
 use serde_json::{self};
+use sha1::{Digest, Sha1};
 use std::{env, error::Error};
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &mut MyBEncodedBuf) -> serde_json::Value {
-    let a = encoded_value.parse().unwrap();
+    let a = encoded_value.decode().unwrap();
     a
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
-fn main()  {
+fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
     if command == "decode" {
@@ -20,7 +21,8 @@ fn main()  {
         let encoded_value = &args[2];
         let mut buf = MyBEncodedBuf::from(encoded_value);
         let decoded_value = decode_bencoded_value(&mut buf);
-        println!("{}", decoded_value.to_string());
+        let _ = buf.encode(&decoded_value);
+        println!("{} {}", encoded_value, decoded_value.to_string());
     } else if command == "info" {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         // println!("Logs from your program will appear here!");
@@ -30,7 +32,8 @@ fn main()  {
         let a = std::fs::read(file_name).unwrap();
         let mut buf = MyBEncodedBuf {
             pos: 0,
-            string_buf: a,
+            inner_buf: a,
+            outer_buf: vec![],
         };
         let decoded_value = decode_bencoded_value(&mut buf);
         let announce = decoded_value
@@ -40,17 +43,41 @@ fn main()  {
             .unwrap()
             .as_str()
             .unwrap();
-        let info = decoded_value
-            .as_object()
-            .unwrap()
-            .get("info")
-            .unwrap()
-            .as_object()
-            .unwrap();
+        let info_v = decoded_value.as_object().unwrap().get("info").unwrap();
+        let info = info_v.as_object().unwrap();
         let length = info.get("length").unwrap().as_i64().unwrap();
+        let _sh1_digest = Sha1::digest(&buf.outer_buf);
+        let hx = hex::encode(_sh1_digest);
+        let _ = buf.encode(&info_v);
         println!("Tracker URL: {:}", announce);
-        println!("Length: {:#?}", length);
+        println!("Length: {:?}", length);
+        println!("Info Hash: {:?}", hx);
     } else {
         println!("unknown command: {}", args[1])
     };
+}
+#[test]
+fn tt() {
+    let encoded_value = "d3:4443:9995:54321d3:3213:3215:54321i99ee3:321l12:000987654321i99eee";
+    let mut buf = MyBEncodedBuf::from(encoded_value);
+    let decoded_value = decode_bencoded_value(&mut buf);
+    let ec = buf.encode(&decoded_value).unwrap();
+    let mut buf2 = MyBEncodedBuf {
+        outer_buf: vec![],
+        pos: 0,
+        inner_buf: buf.outer_buf.clone(),
+    };
+    let decoded_value2 = decode_bencoded_value(&mut buf2);
+
+    println!(
+        "{}   \n {:?}\n {:?}",
+        encoded_value,
+        decoded_value,
+        decoded_value2,
+        // String::from_utf8(buf.outer_buf.clone()).unwrap()
+    );
+    assert_eq!(
+        String::from_utf8(buf2.inner_buf.clone()).unwrap(),
+        String::from_utf8(buf.inner_buf.clone()).unwrap()
+    )
 }
