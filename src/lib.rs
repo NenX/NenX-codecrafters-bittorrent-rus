69232e477fc4d37715f324f532c583dcbbbbe5ab
brainfuck <noah1999@qq.com>
2024-10-06 18:02:07 +0800

    parse list

---
+++ src/lib.rs
@@ -0,0 +1,134 @@
+use std::error::Error;
+
+use serde_json::Value;
+
+pub struct MyBEncodedBuf {
+    pub pos: usize,
+    pub string_buf: String,
+}
+type MyBEncodedResult<T> = Result<T, Box<dyn Error>>;
+impl MyBEncodedBuf {
+    pub fn get_current_slice(&self) -> &str {
+        let a = &self.string_buf[self.pos..];
+        a
+    }
+    pub fn len_bound(&self) -> usize {
+        self.string_buf.len()
+    }
+    pub fn read(&mut self) -> MyBEncodedResult<char> {
+        let ret = self.peek()?;
+        self.seek(self.pos + ret.len_utf8())?;
+
+        Ok(ret)
+    }
+    pub fn peek(&self) -> MyBEncodedResult<char> {
+        if self.pos > self.len_bound() {
+            return Err("read".into());
+        }
+        let mut chars = self.string_buf[self.pos..].chars();
+        let ret = chars.next().ok_or("peek")?;
+
+        Ok(ret)
+    }
+    pub fn seek(&mut self, p: usize) -> MyBEncodedResult<()> {
+        if p > self.len_bound() {
+            return Err("seek".into());
+        }
+        self.pos = p;
+        Ok(())
+    }
+    pub fn step(&mut self, steps: usize) -> MyBEncodedResult<()> {
+        let target = self.pos + steps;
+        if target > self.len_bound() {
+            return Err(format!("pos {} step {}", self.pos, steps).into());
+        }
+        self.pos = target;
+        Ok(())
+    }
+    pub fn parse_str(&mut self) -> MyBEncodedResult<Value> {
+        let a = self.get_current_slice().split_once(':').ok_or("split")?;
+
+        let n = a.0.parse::<usize>()?;
+        let s = a.1[0..n].to_string();
+        self.step(1 + a.0.len() + n)?;
+
+        Ok(s.into())
+    }
+    pub fn parse_integer(&mut self) -> MyBEncodedResult<Value> {
+        self.get_current_slice();
+
+        self.step(1)?;
+
+        let a = self.get_current_slice().split_once('e').ok_or("split")?;
+        let n = a.0.parse::<i64>()?;
+        self.step(a.0.len() + 1)?;
+
+        Ok(n.into())
+    }
+    pub fn parse_list(&mut self) -> MyBEncodedResult<Value> {
+        self.step(1)?;
+        let mut v = vec![];
+        while let Ok(c) = self.peek() {
+            match c {
+                'e' => {
+                    self.step(1)?;
+                    break;
+                }
+                _ if c == 'i' => {
+                    let value = self.parse_integer()?;
+                    v.push(value);
+                    continue;
+                }
+                _ if c.is_digit(10) => {
+                    let value = self.parse_str()?;
+                    v.push(value);
+                    continue;
+                }
+                _ => {
+                    panic!("Unhandled list encoded value: {}", c)
+                }
+            };
+        }
+
+        Ok(v.into())
+    }
+    pub fn parse(&mut self) -> MyBEncodedResult<Value> {
+        let c = self.peek()?;
+        let a = match c {
+            'i' => {
+                let n = self.parse_integer()?;
+                n
+            }
+            'l' => {
+                let n = self.parse_list()?;
+                n
+            }
+
+            _ if c.is_digit(10) => {
+                let n = self.parse_str()?;
+                n
+            }
+
+            _ => {
+                panic!("Unhandled encoded value: {}", self.string_buf)
+            }
+        };
+        Ok(a)
+    }
+}
+impl From<&str> for MyBEncodedBuf {
+    fn from(value: &str) -> Self {
+        Self {
+            pos: 0,
+            string_buf: value.to_owned(),
+        }
+    }
+}
+impl From<&String> for MyBEncodedBuf {
+    fn from(value: &String) -> Self {
+        Self {
+            pos: 0,
+            string_buf: value.to_owned(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,26 @@
+use bittorrent_starter_rust::MyBEncodedBuf;
 use serde_json::{self, Number};
 use std::env;
 
-// Available if you need it!
-// use serde_bencode
-
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    let len = encoded_value.len();
-    let c = encoded_value.chars().next().unwrap();
-    let a = match c {
-        'i' if encoded_value.ends_with('e') => {
-            let n_str = &encoded_value[1..len - 1];
-            let n = n_str.parse::<i64>().unwrap();
-            serde_json::Value::Number(Number::from(n))
-        }
-
-        _ if c.is_digit(10) => {
-            let colon_index = encoded_value.find(':').unwrap();
-            let number_string = &encoded_value[..colon_index];
-            let number = number_string.parse::<i64>().unwrap();
-            let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-            serde_json::Value::String(string.to_string())
-        }
-
-        _ => {
-            panic!("Unhandled encoded value: {}", encoded_value)
-        }
-    };
+fn decode_bencoded_value(encoded_value: &mut MyBEncodedBuf) -> serde_json::Value {
+    let a = encoded_value.parse().unwrap();
     a
-    // If encoded_value starts with a digit, it's a number
-    // if encoded_value.chars().next().unwrap().is_digit(10) {
-    //     // Example: "5:hello" -> "hello"
-    //     let colon_index = encoded_value.find(':').unwrap();
-    //     let number_string = &encoded_value[..colon_index];
-    //     let number = number_string.parse::<i64>().unwrap();
-    //     let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-    //     return serde_json::Value::String(string.to_string());
-    // } else {
-    //     panic!("Unhandled encoded value: {}", encoded_value)
-    // }
+
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
-
     if command == "decode" {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         // println!("Logs from your program will appear here!");
 
         // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
+        let mut buf = MyBEncodedBuf::from(encoded_value);
+        let decoded_value = decode_bencoded_value(&mut buf);
         println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
