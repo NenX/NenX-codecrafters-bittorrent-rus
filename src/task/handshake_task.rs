48c8faeec7368c10acb0dd32f70cb63058f58e04
brainfuck <noah1999@qq.com>
2024-10-16 14:06:18 +0800

    Empty-Commit

--- src/task/handshake_task.rs
+++ src/task/handshake_task.rs
@@ -12,8 +12,9 @@ pub async fn handshake_task<T: AsRef<Path>>(torrent: T, peer: &str) -> Result<My
 }
 pub async fn magnet_handshake_task(link: &str) -> Result<()> {
     let mag = MyMagnet::from_link(link)?;
-    let _ins = MyConnect::magnet_info(&mag).await?;
+    let mut conn = MyConnect::magnet_handshake(&mag).await?;
 
+    let (mut peer_framed, payload) = conn.magnet_pre_download().await?;
     Ok(())
 }
 pub async fn magnet_parse_info(link: &str) -> Result<()> {
